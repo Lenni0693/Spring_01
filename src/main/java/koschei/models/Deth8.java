8d339238511b32e38627f9049252180e19316eb0
Lenie <Lenni0693@gmail.com>
2021-10-04 01:36:13 +0300

    2_1_4

---
+++ src/main/java/koschei/models/Deth8.java
@@ -0,0 +1,13 @@
+package koschei.models;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Deth8 {
+
+
+    @Override
+    public String toString() {
+        return "вот и сказочке конец, а кто слушал - молодец)";
+    }
+}
