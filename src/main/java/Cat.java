157cca6e02e54b6188ec6dbd9d69da65bb1ab1f1
Lenie <Lenni0693@gmail.com>
2021-10-04 00:09:50 +0300

    spring

---
+++ src/main/java/Cat.java
@@ -0,0 +1,10 @@
+public class Cat {
+    private String catName;
+
+    public String getName() {
+        return catName;
+    }
+    public void setName (String name){
+        this.catName = name;
+    }
+}
